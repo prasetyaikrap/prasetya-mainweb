5a282632250daa52e8c31838b8f608ee8d6a1fb7
Prasetya Ikra Priyadi <prasetya.ikrapriyadi@gmail.com>
2022-02-23 21:06:42 +0700

    Adding Skill section with CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,14 @@ import React from "react";
 //components
 import Navbar from "./Navbar";
 import ProfileSec from "./Profile";
+import Skill from "./Skill";
 
 function Home() {
   return (
     <div className="home-body">
       <Navbar />
       <ProfileSec />
+      <Skill />
     </div>
   );
 }
